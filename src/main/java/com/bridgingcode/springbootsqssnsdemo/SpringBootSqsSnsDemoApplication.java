ad4ce143a68ac516fa06e6fd4c8e3ee943089b3a
AJ Catambay <aj.bridgingcode@gmail.com>
2020-10-21 11:24:01 +0800

    initial commit

---
+++ src/main/java/com/bridgingcode/springbootsqssnsdemo/SpringBootSqsSnsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bridgingcode.springbootsqssnsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSqsSnsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSqsSnsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgingcode/springbootsqssnsdemo/SpringBootSqsSnsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgingcode.springbootsqssnsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSqsSnsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
